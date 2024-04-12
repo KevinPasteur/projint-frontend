599315a222cbad98e28191823e0bc9104aff90ae
Kévin <kevin.pasteur@icloud.com>
2024-04-12 14:16:23 +0200

    initial setup

---
+++ src/components/ConnectionManager.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="connect()">Connect</button>
+    <button @click="disconnect()">Disconnect</button>
+    <button @click="createRoom()">Create Room</button>
+</template>
+
+<script>
+import { socket } from "@/socket";
+
+export default {
+    name: "ConnectionManager",
+
+    methods: {
+        connect() {
+
+            socket.connect();
+            console.log(socket.connect());
+        },
+        createRoom() {
+            socket.emit('create', 'room1');
+        },
+        disconnect() {
+            socket.disconnect();
+            console.log(socket.disconnect());
+        }
+    }
+}
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ConnectionManager from '@/components/ConnectionManager.vue';
+import ConnectionState from '@/components/ConnectionState.vue';
+</script>
+
+<template>
+    <main>
+        <ConnectionManager />
+        <ConnectionState />
+    </main>
+</template>
