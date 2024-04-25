9f5ba52dc0d23a340f829859eb99b11552121923
Kévin <kevin.pasteur@icloud.com>
2024-04-25 02:22:16 +0200

    Update URL

--- src/socket.js
+++ src/socket.js
@@ -6,10 +6,7 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : "https://academic-brandie-unbored.koyeb.app";
+const URL = "https://academic-brandie-unbored.koyeb.app";
 
 export const socket = io(URL);
 
